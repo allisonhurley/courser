106293c7ad380272a545168945574da72403c72c
Allison Hurley <allison@hurleyhome.com>
2018-08-26 19:33:42 -0400

    Added Professors

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_26_232714) do
+ActiveRecord::Schema.define(version: 2018_08_26_233230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "professors", force: :cascade do |t|
+    t.string "last_name"
+    t.string "first_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "last_name"
